a239d5e11eb733e767ef6a75a7da0639c4ed1fe6
adevmir <ademirvalonqar@gmail.com>
2022-04-06 19:51:33 -0300

    [correcao 03 - lint]

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -4,10 +4,18 @@ class MissionCard extends React.Component {
   render() {
     return (
       <div data-testid="mission-card">
-        <p data-testid="mission-name"> { this.props.name } </p>
-        <p data-testid="mission-year"> { this.props.year }</p>
-        <p data-testid="mission-country"> { this.props.country }</p>
-        <p data-testid="mission-destination"> { this.props.destination }</p>
+        <p data-testid="mission-name">
+          { this.props.name }
+        </p>
+        <p data-testid="mission-year">
+          { this.props.year }
+        </p>
+        <p data-testid="mission-country">
+          { this.props.country }
+        </p>
+        <p data-testid="mission-destination">
+          { this.props.destination }
+        </p>
       </div>
     );
   }
